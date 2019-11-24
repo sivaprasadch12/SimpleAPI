600bee5859a9c88a7d6333aa7b3fdb999c359d99
$CDWebAPP20191117111829 <sivaprasad.chennareddy@bp.com>
2019-11-24 13:52:20 -0600

    Revert the Changes

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -39,7 +39,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "sivaprasad";
+            return "siva";
         }
     }
 }
