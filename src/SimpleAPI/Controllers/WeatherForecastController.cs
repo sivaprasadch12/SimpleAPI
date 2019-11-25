8da2d92e8afe709389ee8cc3eb1a45c690437965
$CDWebAPP20191117111829 <sivaprasad.chennareddy@bp.com>
2019-11-25 13:57:43 -0600

    Modified Summary

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -29,8 +29,8 @@ namespace SimpleAPI.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                
+                
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
