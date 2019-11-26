543444ae7bf521fb105935c801c416b12ff244b6
$CDWebAPP20191117111829 <sivaprasad.chennareddy@bp.com>
2019-11-26 15:33:41 -0600

    CodeChanged

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -38,7 +38,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "sivap";
+            return "siva";
         }
     }
 }
